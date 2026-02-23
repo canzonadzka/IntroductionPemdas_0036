e328c0326025ff4e183db81707559a24f95fea8a
canzonadzka <canzonadzka@gmail.com>
2026-02-23 12:14:45 +0700

    Selesai

--- pemdas2.cpp
+++ pemdas2.cpp
@@ -3,23 +3,25 @@ using namespace std;
 
 int main()
 {
-}
 // Langkah 1
 int luas, panjang, lebar;
 
 // Langkah 2
-cout << "Masukkan Panjang :";
-cin >> Panjang;
+cout << "Masukkan panjang :";
+cin >> panjang;
 
 // Langkah 3
-cout << "Masukkan Lebar :";
-cin >> lebar
+cout << "Masukkan lebar :";
+cin >> lebar;
 
 // Langkah 4
 luas = panjang * lebar;
 
 // Langkah 5
 cout << "Hasilnya : " << luas;
+
+//selesai
+}
 
 
 
